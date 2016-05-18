6fa94d85a271aee30b29c04a76d0fe69f0e1a321
alexjhannan <alexjhannan@gmail.com>
2016-05-18 14:17:49 -0400

    Styled navbar

--- index.android.js
+++ index.android.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, StyleSheet } from 'react-native';
 import { Scene, Router } from 'react-native-router-flux';
 
 import Home from './components/home.js';
@@ -8,12 +8,27 @@ import Pomodoro from './components/pomodoro.js';
 class App extends React.Component {
   render() {
     return <Router>
-      <Scene key="root">
+      <Scene key="root" navigationBarStyle={styles.navBar} titleStyle={styles.navBarTitle} barButtonTextStyle={styles.barButtonTextStyle} barButtonIconStyle={styles.barButtonIconStyle}>
         <Scene key="home" component={Home} title="Home" initial={true}/>
         <Scene key="pomodoro" component={Pomodoro} timerDuration={.1} title="Pomodoro"/>
       </Scene>
     </Router>
   }
 }
+
+const styles = StyleSheet.create({
+  navBar: {
+    backgroundColor:'#224422',
+  },
+  navBarTitle:{
+      color:'#DDEEDD'
+  },
+  barButtonTextStyle:{
+      color:'#FFFFFF'
+  },
+  barButtonIconStyle:{
+      tintColor:'#EEFFEE'
+  },
+});
 
 AppRegistry.registerComponent('marblehead', () => App);
